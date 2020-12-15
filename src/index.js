f6ae80765bf5f3fe0475e1c9a800260c6442152d
hyewonji <60416187+hyewonji@users.noreply.github.com>
2020-12-15 23:44:04 +0900

    test login button onclick event and animation

---
+++ src/index.js
@@ -0,0 +1,7 @@
+function check(form) {
+    if (form.userId.value == "hyewon" && form.password.value == "1234") {
+        console.log("welcome!")
+    } else {
+        console.log("try again!")
+    }
+}
